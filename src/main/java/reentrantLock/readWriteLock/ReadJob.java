b2d79688ca9f54d66ddf8239ab68506bb4e90b14
suoer <suoer@mogujie.com>
2017-04-12 12:13:14 +0800

    从内存模型 程序内存分配空间 理解多线程 锁的问题

--- src/main/java/reentrantLock/readWriteLock/ReadJob.java
+++ src/main/java/reentrantLock/readWriteLock/ReadJob.java
@@ -2,6 +2,8 @@ package reentrantLock.readWriteLock;
 
 import reentrantLock.PrintQueue;
 
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 /**
  * Created by mortli on 4/11/17.
  */
@@ -16,10 +18,15 @@ public class ReadJob implements Runnable {
     @Override
     public void run() {
 
+//        ReentrantReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+//        readWriteLock.readLock().lock();
+//  锁必须在一个对象里  一个内存空间 作为竞态资源共享 多线程之间一定要是共享的  否则无任何意义
+// 必须在堆区  这样在方法栈里 是无法共享的 错的离谱
+
         System.out.println(" read a " +Thread.currentThread().getName()+"  "+  prieceInfo.getPrieceA());
 
         System.out.println(" read b " +Thread.currentThread().getName()+"   " + prieceInfo.getPrieceB());
-
+//        readWriteLock.readLock().unlock();
 
     }
 }
