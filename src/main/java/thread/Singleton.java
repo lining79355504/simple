dfea06b53f0d243f13f580321048b04ef8b3b033
mort <mortaceli@gmail.com>
2017-06-30 18:58:58 +0800

    commit

---
+++ src/main/java/thread/Singleton.java
@@ -0,0 +1,38 @@
+package thread;
+
+/**
+ * Created by mort on 16/06/2017.
+ */
+public class Singleton {
+
+    private static volatile Singleton instance = null;
+
+    private Singleton() {
+    }
+
+    public static Singleton getInstance() {
+        Singleton ins = instance; //增加临时变量
+        if (ins == null) {
+            synchronized (Singleton.class) {
+                ins = instance;
+                if (ins == null) {
+                    ins = instance = new Singleton();
+                }
+            }
+        }
+        return ins;
+    }
+
+    public static Singleton normalInstance(){
+
+        if (instance == null) {
+            synchronized (Singleton.class) {
+                if (instance == null) {
+                    instance = new Singleton();
+                }
+            }
+        }
+        return instance;
+    }
+
+}
